0056467a8ba916140d91f67a755382ca03273fb3
gandalfgreyhem5070 <ashleyaritchie@gmail.com>
2021-10-04 18:07:48 -0400

    COmpleted script.js build. File complete
    '

--- script.js
+++ script.js
@@ -33,3 +33,43 @@ OR
 ${enter}`;
 const gameOver5 = `You looked at the trap and the light BLINDED YOU! ${gameOver}`;
 const win = `The ghost trap sucked Slimer in and closed. CONGRATULATIONS! YOU CAUGHT SLIMER!!!`;
+
+// Alerts user to start game 
+alert(start);
+
+// sets value of userInput to the users enrty
+let userInput = prompt(q1);
+ 
+// 1st CONDTIONAL STATEMENT
+if (userInput == 1)  {
+   userInput = prompt(q2);
+//2ND CONDTIONSL STATEMENT
+   if (userInput == 1) {
+       userInput = prompt(q3);
+//3RD CONDITIONAL STATEMENT
+    if (userInput == 2) {
+        userInput = prompt(q4);
+//4TH CONDTIONAL STATEMENT
+        if (userInput == 1){
+            userInput = prompt(q5);
+//5TH CONDITIONAL STATEMENT
+            if (userInput == 2){
+                alert(win);
+            }else{
+                alert(gameOver5);
+            }
+        }else {
+            alert(gameOver4);
+        }
+
+    } else {
+        alert(gameOver3);
+    }
+   } else {
+       alert(gameOver2);
+   }
+} else {
+    alert(gameOver1);
+}
+
+
